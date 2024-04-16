bcff7a04d340012e4ea01581f11a7c4dce36ea22
13NOONE37 <oliwerklauze22@gmail.com>
2024-04-16 13:37:01 +0200

    char escape added

--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -34,7 +34,7 @@ const Footer: FC = () => {
           Audiophile is an all in one stop to fulfill your audio needs. We're a
           small team of music lovers and sound specialists who are devoted to
           helping you get the most out of personal audio. Come and visit our
-          demo facility - we're open 7 days a week.
+          demo facility - we&apos;re open 7 days a week.
         </p>
         <span className={styles.copyright}>
           Copyright 2021. All Rights Reserved
