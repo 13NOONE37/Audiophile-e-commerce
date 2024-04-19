f68f1d2b3466fcef6aa6659cb9e3c2150c994e8c
13NOONE37 <oliwerklauze22@gmail.com>
2024-04-19 11:23:13 +0200

    deploy on netlify config changes

--- api/config/plugins.js
+++ api/config/plugins.js
@@ -1,1 +1,16 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  "netlify-deployments": {
+    enabled: true,
+    config: {
+      accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
+    },
+    sites: [
+      {
+        name: "audiophile-eco",
+        id: "9560f2f9-2e55-4205-b28c-fed874477078",
+        buildHook: process.env.NETLIFY_BUILD_HOOK,
+        branch: "main",
+      },
+    ],
+  },
+});
